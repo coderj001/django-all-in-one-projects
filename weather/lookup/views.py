f657be07f726b5fdaf86096d491476fa06afa2e3
coderj001 <amirajubolchi001@gmail.com>
2020-01-14 21:05:34 +0530

    API is added and include in templates

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
 # Create your views here.
 def home(request):
-	return render(request,'home.html',{})
+	import json
+	import requests
+	api_req=requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20001&distance=25&API_KEY=EABC3623-4BCF-4437-8D1B-B515131C0BC7').content
+	try:
+		api=json.loads(api_req)
+	except Exception as e:
+		api="ERROR...."
+	return render(request,'home.html',{'api':api})
 def about(request):
 	return render(request,'about.html',{})
